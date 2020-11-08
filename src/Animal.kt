7c5440f3b69a10e6fe234f45553ee5c3fd8dde37
ScottGledhill <scottgledhill@hotmail.co.uk>
2020-11-08 17:59:25 +0000

    created animal superclass

---
+++ src/Animal.kt
@@ -0,0 +1,14 @@
+open class Animal(val image: String, val food: String, val habitat: String, val hunger: Int) {
+    fun makeNoise() {
+        println("roar")
+    }
+    fun eat() {
+        println("eat ")
+    }
+    fun roam() {
+        println("roam")
+    }
+    fun sleep() {
+        println("sit down bitch")
+    }
+}
